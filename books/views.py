88bcd2ed6b2e7af5547ed7f8eb3230538146457c
G_alileo <jamespmurithi@gmail.com>
2025-03-23 20:14:33 +0300

    Implementing views for CRUD operations

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        """Restrict certain actions to admins only"""
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAdminUser()]  # Only admins can modify books
+        return [permissions.AllowAny()]  # Anyone can view books
