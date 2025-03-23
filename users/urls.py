daf155e13d2bc537a9931e09d672dba279272c10
G_alileo <jamespmurithi@gmail.com>
2025-03-23 20:52:40 +0300

    Registering the newly added api end points for the user management

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from django.urls import path, include
+from .views import RegisterView, LoginView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)  # Registers the user API
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
 ]
