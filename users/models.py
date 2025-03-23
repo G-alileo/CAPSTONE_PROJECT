904707ac8528f3cdc226a476d4b5c89f1f57c5dd
G_alileo <jamespmurithi@gmail.com>
2025-03-23 18:19:13 +0300

    Adding a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_membership = models.DateTimeField(default=now)
+    is_active = models.BooleanField(default=True)
+    ROLE_CHOICES = (("admin", "Admin"), ("user", "User"))
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="user")
+
+    def __str__(self):
+        return self.username
