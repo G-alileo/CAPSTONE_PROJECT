4615cbbeff2691ea2adccdd8cb73e03054e97299
G-alileo <jamespmurithi@gmail.com>
2025-03-29 10:26:51 +0300

    Creation of a transaction model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -10,3 +14,12 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} borrowed {self.book.title}"
