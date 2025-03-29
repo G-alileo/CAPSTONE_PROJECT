1eaec886e9d82f26a0b206f948bbef266506d4b7
G-alileo <jamespmurithi@gmail.com>
2025-03-29 10:34:52 +0300

    regisstrering the end points

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, TransactionViewSet
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)  # Registers all book-related routes
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('checkout/<int:book_id>/', TransactionViewSet.as_view({'post': 'checkout_book'}), name='checkout-book'),
+    path('return/<int:book_id>/', TransactionViewSet.as_view({'post': 'return_book'}), name='return-book'),
+
 ]
